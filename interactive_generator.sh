6f09bbd62991b13227c1b5a729c7841354419cda
LeixB <abone9999@gmail.com>
2023-09-16 20:43:38 +0200

    Fix script

--- interactive_generator.sh
+++ interactive_generator.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-FILE="${1:-data.json}"
+FILE="${FILE:-./data.json}"
+INPUT_FILE="${1:-$FILE}"
 
-UCIML=./uciml
+UCIML="${UCIML:-./uciml}"
 
-$UCIML --serve "$FILE" >/dev/null 2>&1 &
+$UCIML --serve "${INPUT_FILE}" >/dev/null 2>&1 &
 pid=$!
 
 sleep 1 # wait for server to start
 
+mkdir -p generated
+
 curl -H 'Accept: text/plain' http://localhost:8080/datasets | \
     fzf --multi --preview "curl -s -H 'Accept: text/markdown' http://localhost:8080/datasets/{2} | bat -l md --color always" | \
     cut -d' ' -f2 | tee generated/selection.txt | \
-    xargs $UCIML --verbose --julia --toml data.json
+    xargs "$UCIML" --verbose --julia --toml "${INPUT_FILE}"
 
 kill $pid
